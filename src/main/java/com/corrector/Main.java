a50c7fe4a901841a9259e363d8afc0854fc7da3b
Mahefaa <hei.mahefa@gmail.com>
2025-02-13 16:06:47 +0300

    feat: correct apps from jc-db and write scores

--- src/main/java/com/corrector/Main.java
+++ src/main/java/com/corrector/Main.java
@@ -1,7 +1,26 @@
 package com.corrector;
 
+import com.corrector.dataproviders.CsvDataProvider;
+import com.corrector.dataproviders.DbDataProvider;
+import com.corrector.fileLoader.ClasspathResourceLoader;
+import com.corrector.model.evaluated.CorrectedApp;
+import com.corrector.scoreWriter.ScoreWriter;
+import java.util.Map;
+
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader();
+    var dbDataProvider = new DbDataProvider(resourceLoader);
+    var csvDataProvider = new CsvDataProvider(resourceLoader);
+    var deployedApps = dbDataProvider.apply("prod-jcloudify_apps_2025-02-13_14-53-29.json");
+    var students = csvDataProvider.apply("answers_jo_2024_feb13_11_51.csv");
+
+    Corrector corrector = new Corrector();
+    Map<String, CorrectedApp> correctedApps = corrector.computeScoreByStudent(deployedApps);
+
+    ScoreWriter scoreWriter = new ScoreWriter();
+    scoreWriter.writeScores(students, correctedApps);
+  }
 }
